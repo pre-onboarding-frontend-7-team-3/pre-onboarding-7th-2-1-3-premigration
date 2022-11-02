23a732276ff6dc9488247d64a646dc509d9cf2b2
Sangmin <23mean@naver.com>
2022-11-02 21:35:47 +0900

    Feat: GlobalStyle 수정 및 Theme 추가

--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router } from "react-router-dom";
+import { ThemeProvider } from "styled-components";
+
 import "./index.css";
 import App from "./App";
-import { BrowserRouter as Router } from "react-router-dom";
+
+import GlobalStyle from "./styles/GlobalStyle";
+import Theme from "./styles/Theme";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Router>
-    <App />
+    <GlobalStyle />
+    <ThemeProvider theme={Theme}>
+      <App />
+    </ThemeProvider>
   </Router>,
 );
