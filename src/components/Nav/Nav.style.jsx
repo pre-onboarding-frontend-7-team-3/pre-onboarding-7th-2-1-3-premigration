fb921530389c01f25573734dc014652fa309a3b8
Sangmin <23mean@naver.com>
2022-11-02 21:36:41 +0900

    Fix: Nav Style 수정

--- src/components/Nav/Nav.style.jsx
+++ src/components/Nav/Nav.style.jsx
@@ -2,10 +2,32 @@ import styled from "styled-components";
 
 const NavWrapper = styled.div`
   display: flex;
-  justify-content: space-around;
+  align-items: center;
+
+  width: 100%;
+  max-width: 450px;
+  gap: 20px;
+
+  padding: 8px 0;
+
+  border-bottom: 1px solid ${(props) => props.theme.BLACK};
+
+  overflow-x: auto;
+  &::-webkit-scrollbar {
+    display: none;
+  }
+  @media only screen and (max-width: 400px) {
+    justify-content: flex-start;
+  }
 `;
 
 const NavItemStyle = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  margin: 0 12px;
+
   cursor: pointer;
 `;
 
--- src/components/Nav/NavItem.jsx
+++ src/components/Nav/NavItem.jsx
@@ -2,13 +2,21 @@ import { CarContext } from "context/CarContext";
 import { useContext } from "react";
 import { NavItemStyle } from "./Nav.style";
 
+import Button from "../common/Button";
+
 function NavItem({ text, type }) {
   const { getCars } = useContext(CarContext);
   const handleNavBox = () => {
     getCars({ segment: type });
     console.log("nav type", type);
   };
-  return <NavItemStyle onClick={handleNavBox}>{text}</NavItemStyle>;
+  return (
+    <NavItemStyle>
+      <Button borderRadius="62px" onClick={handleNavBox}>
+        {text}
+      </Button>
+    </NavItemStyle>
+  );
 }
 
 export default NavItem;
