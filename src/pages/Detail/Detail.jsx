bedac0096657132c8c484cedd5d514a258e91609
YeonghunKO <yhko1988@gmail.com>
2022-11-03 07:50:39 +0900

    Feat : Detail.jsx 로 확장자 변경

---
+++ src/pages/Detail/Detail.jsx
@@ -0,0 +1,33 @@
+import Header from "components/Header";
+import { CarContext } from "context/CarContext";
+import { useContext, useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
+function Detail() {
+  const { id } = useParams();
+  const {
+    carState: { carList, selectedCar },
+    findCars,
+    isLoading,
+  } = useContext(CarContext);
+
+  useEffect(() => {
+    findCars(id);
+  }, [carList]);
+
+  return (
+    <section>
+      <Header title="차량상세" />
+      {selectedCar?.length && (
+        <>
+          <div>
+            {selectedCar[0]?.attribute.brand}
+            {selectedCar[0]?.attribute.name}
+          </div>
+        </>
+      )}
+    </section>
+  );
+}
+
+export default Detail;
