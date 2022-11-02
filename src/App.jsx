f7cd1ddd781de95cb3082bf93e2fe8b79245eac4
suzz-in <o_o_su_jin_@naver.com>
2022-11-02 16:27:13 +0900

    feat : header 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,29 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { useMediaQuery } from "react-responsive";
+import Detail from "./pages/Detail";
 import Home from "./pages/Home";
 
 function App() {
+  const Desktop = ({ children }) => {
+    const isDesktop = useMediaQuery({ minWidth: 451 });
+    return isDesktop ? children : null;
+  };
+
+  const Mobile = ({ children }) => {
+    const isMobile = useMediaQuery({ maxWidth: 450 });
+    return isMobile ? children : null;
+  };
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/home" element={<Home />} />
-      </Routes>
+      <Desktop>
+        <h1>화면을 줄여 이용해주세요</h1>
+      </Desktop>
+      <Mobile>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/detail:id" element={<Detail />} />
+        </Routes>
+      </Mobile>
     </BrowserRouter>
   );
 }
