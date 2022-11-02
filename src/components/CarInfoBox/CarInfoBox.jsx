1af8442306346a35aa21285a903d7979ed9e0c3c
Sangmin <23mean@naver.com>
2022-11-02 21:36:07 +0900

    Fix: CarInfoBox Style 수정

--- src/components/CarInfoBox/CarInfoBox.jsx
+++ src/components/CarInfoBox/CarInfoBox.jsx
@@ -1,12 +1,38 @@
+<<<<<<< main
 import { CarInfoBoxStyle } from "./CarInfoBox.style";
 import isNew from "utils/isNew";
 function CarInfoBox({ attribute: { brand, name }, createdAt}) {
   console.log(isNew(createdAt))
+=======
+import Button from "../common/Button";
+import * as Style from "./CarInfoBox.style";
+
+function CarInfoBox({ attribute: { brand, name } }) {
+>>>>>>> Fix: CarInfoBox Style 수정
   return (
-    <CarInfoBoxStyle>
-      {brand}
-      {name}
-    </CarInfoBoxStyle>
+    <Style.Wrapper>
+      <Style.InnerWrapper>
+        <Style.TextWrapper>
+          <Style.MainInfo>{brand}</Style.MainInfo>
+          <Style.SubInfo>{name}</Style.SubInfo>
+        </Style.TextWrapper>
+        <Style.ImageWrapper>
+          <Style.ButtonWrapper>
+            <Button
+              width="52px"
+              height="22px"
+              borderRadius="42px"
+              fontSize="12px"
+              fontWeight="700"
+              color={(props) => props.theme.WHITE}
+              backgroundColor={(props) => props.theme.BLUE}
+            >
+              신규
+            </Button>
+          </Style.ButtonWrapper>
+        </Style.ImageWrapper>
+      </Style.InnerWrapper>
+    </Style.Wrapper>
   );
 }
 
