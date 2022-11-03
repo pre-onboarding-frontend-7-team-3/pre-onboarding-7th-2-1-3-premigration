875cc0af4066a675b2ef3075cd4a74f23b143552
YeonghunKO <yhko1988@gmail.com>
2022-11-03 14:14:00 +0900

    Feat : Detail페이지 완료

---
+++ src/components/CarDetail/CarDetailData.js
@@ -0,0 +1,11 @@
+export const CAR_DETAIL_CATEGORY = {
+  DETAIL_INFO: "차량 정보",
+  INSURANCE: "보험",
+  OPTIONS: "추가상품",
+};
+
+export const CAR_DETAIL_INFO = {
+  SEGMENT: "차종",
+  FUELTYPE: "연료",
+  AVAILABLE_DATE: "이용 가능일",
+};
---
+++ src/components/CarDetail/CarDetailItem.jsx
@@ -0,0 +1,15 @@
+import * as S from "./CarDetail.style";
+
+const CarDetailItem = ({ name, description, date, amount }) => {
+  console.log(amount)
+  return (
+    <S.TextWrapper>
+      <S.AttributeSubCategory>{name}</S.AttributeSubCategory>
+      {description && <S.SubAttribute>{description}</S.SubAttribute>}
+      {date && <S.SubAttribute>{date}</S.SubAttribute>}
+      {amount && <S.SubAttribute>월 {amount.toLocaleString("ko-KR")} 원</S.SubAttribute>}
+    </S.TextWrapper>
+  );
+};
+
+export default CarDetailItem;
