f2458bb980cb5d60d25b7592aa9fb6462d60dffe
Sangmin <23mean@naver.com>
2022-11-03 12:50:59 +0900

    Fix: Header에 사용될 Arrow SVG추가 및 사용되지 않는 가짜 Header 제거

--- src/components/LayOut/LayOut.jsx
+++ src/components/LayOut/LayOut.jsx
@@ -1,10 +1,14 @@
 import { Outlet } from "react-router-dom";
-import styled from "styled-components";
+
 import { LayOutHeader } from "./LayOut.style";
+import Icon from "../common/Icon";
 
 function LayOut({ children }) {
   return (
     <div>
+      {/* FIXME */}
+      {/* 상세 페이직 추가되면 경우에 따라 뒤로가기 버튼을 동작하게 하여 추가 */}
+      {/* <Icon icon="Arrow" /> */}
       <LayOutHeader>전체 차량</LayOutHeader>
       {children || <Outlet />}
     </div>
