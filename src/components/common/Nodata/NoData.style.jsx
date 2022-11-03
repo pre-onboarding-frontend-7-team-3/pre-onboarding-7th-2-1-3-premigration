72d78ed5952c2219c5d2964ddd80b3350c70c402
YeonghunKO <yhko1988@gmail.com>
2022-11-03 15:44:03 +0900

    Refactor : common 폴더 구조 변경에 따른 경로 변경

---
+++ src/components/common/Nodata/NoData.style.jsx
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  height: 100vh;
+
+  font-size: 17px;
+  font-weight: 700;
+  line-height: 21px;
+`;
+
+export { Wrapper };
