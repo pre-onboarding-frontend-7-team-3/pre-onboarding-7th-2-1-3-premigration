f3fdffefd29005434b9b44032f1ec3b24fd39fca
YeonghunKO <yhko1988@gmail.com>
2022-11-03 10:17:04 +0900

    Feat : Header에 Backbutton추가

--- src/components/Header/Header.style.jsx
+++ src/components/Header/Header.style.jsx
@@ -1,7 +1,9 @@
 import styled from "styled-components";
 
 const HeaderStyle = styled.header`
+  position: relative;
   display: flex;
+  align-items: center;
   color: blueviolet;
   justify-content: center;
   font-size: 30px;
